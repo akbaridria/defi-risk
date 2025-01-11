dcfa52c8261c5d14dad5748e4c841b1c6df63a42
Akbar Idria <akbaridria15@gmail.com>
2025-01-12 02:17:02 +0700

    Add theme provider and header components; update configurations and utilities

---
+++ components/header.tsx
@@ -0,0 +1,24 @@
+import { GithubIcon, ShieldBanIcon } from "lucide-react";
+import { ThemeChanger } from "./theme-changer";
+import { Button } from "./ui/button";
+
+const Header = () => {
+	return (
+		<div className="border-b sticky top-0 p-4">
+			<div className="container mx-auto flex items-center justify-between">
+				<div className="flex items-center space-x-2">
+                    <ShieldBanIcon size={24} />
+                    <div className="text-2xl font-semibold tracking-tight">DefiRisk</div>
+                </div>
+				<div className="flex items-center space-x-2">
+					<Button variant="ghost" size="icon">
+						<GithubIcon />
+					</Button>
+					<ThemeChanger />
+				</div>
+			</div>
+		</div>
+	);
+};
+
+export default Header;
